be6f783931375287b7f88278b3d2c5755e8e2b52
GDAMS <irene.femenia@gmail.com>
2026-04-27 17:35:19 +0200

    Adjust military counter calculations and visibility logic

--- main.js
+++ main.js
@@ -15,22 +15,18 @@ window.addEventListener('scroll', function(){
 
 function update_military_counter() {
   if (military_viewable()) {
-    if (military_counter_viewable()) {
-      // 1 pixel = $500,000
-      let militarySpent = (window.scrollX - military.offsetLeft + 175) * 500000;
-      if (militarySpent < 0) militarySpent = 0;
-      if (militarySpent > 2887000000000) militarySpent = 2887000000000;
-      military_counter.innerHTML = money.format(militarySpent);
-    } else {
-      military_counter.innerHTML = '';
-    }
+    let militarySpent = (window.scrollX - military.offsetLeft + 175) * 29740;
+    if (militarySpent < 0) militarySpent = 0;
+    if (militarySpent > 2887000000000) militarySpent = 2887000000000;
+    military_counter.innerHTML = money.format(militarySpent);
+    military_counter.classList.add('visible');
+  } else {
+    military_counter.classList.remove('visible');
+    military_counter.innerHTML = '';
   }
 
   function military_viewable() {
-    return window.scrollX < military.offsetLeft + military.offsetWidth + 100;
-  }
-
-  function military_counter_viewable() {
-    return militaryCounterStart.offsetLeft - window.scrollX < window.innerWidth;
+    return window.scrollX >= military.offsetLeft - window.innerWidth &&
+           window.scrollX < military.offsetLeft + military.offsetWidth + 100;
   }
 }
