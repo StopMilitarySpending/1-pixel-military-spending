4d8a7c445d0b9e503935750458d36edf2778bf69
GDAMS <irene.femenia@gmail.com>
2026-04-27 17:18:57 +0200

    Refactor military counter calculation logic

--- main.js
+++ main.js
@@ -9,8 +9,6 @@ var money = new Intl.NumberFormat('en-US', {
   maximumFractionDigits: 0,
 });
 
-var thousand = new Intl.NumberFormat('en-US');
-
 window.addEventListener('scroll', function(){
   update_military_counter();
 });
@@ -18,22 +16,21 @@ window.addEventListener('scroll', function(){
 function update_military_counter() {
   if (military_viewable()) {
     if (military_counter_viewable()) {
-      // Calculate based on scroll position
-      // Every 2 pixels = $1 million (since 10 pixels = $5 million)
+      // 1 pixel = $500,000
       let militarySpent = (window.scrollX - military.offsetLeft + 175) * 500000;
-      let displayValue = (militarySpent < 2710000000000) ? money.format(militarySpent) : "$2,710,000,000,000.00";
-      military_counter.innerHTML = displayValue;
-    }
-    else {
+      if (militarySpent < 0) militarySpent = 0;
+      if (militarySpent > 2887000000000) militarySpent = 2887000000000;
+      military_counter.innerHTML = money.format(militarySpent);
+    } else {
       military_counter.innerHTML = '';
     }
   }
-  
+
   function military_viewable() {
     return window.scrollX < military.offsetLeft + military.offsetWidth + 100;
   }
-  
+
   function military_counter_viewable() {
-    return militaryCounterStart.offsetLeft - window.scrollX < (window.innerWidth);
+    return militaryCounterStart.offsetLeft - window.scrollX < window.innerWidth;
   }
 }
